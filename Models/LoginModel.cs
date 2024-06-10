c29c2da1cb462ce6b37fd11947343f2c1fac9eef
nuch0208 <nutchanet2887@gmail.com>
2024-06-10 10:23:08 +0700

    first commit

---
+++ Models/LoginModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BlazingBlog.Models
+{
+    public class LoginModel
+    {
+        [Required, EmailAddress, DataType(DataType.EmailAddress)]
+        public string Username { get; set; }  
+        [Required, MinLength(5)]
+        public string Password { get; set; }  
+    }
+
+  
+}
